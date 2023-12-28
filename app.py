faae8f344b1401bfdf6b0901d44ea37cb8652110
Rabbi Hossain Rakib <rrakibb.rh@gmail.com>
2023-12-28 09:36:44 +0600

    adding all the practices

---
+++ app.py
@@ -0,0 +1,15 @@
+# getting started
+# print("hello World")
+# x = (2 + 1)
+# print(x)
+
+# Variables
+
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = """
+Multiple 
+lines
+"""
+x, y = 1, 2
