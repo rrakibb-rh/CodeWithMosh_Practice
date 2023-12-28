874bfb09eff80cb531dd1359e060a1abf020f51d
Rabbi Hossain Rakib <rrakibb.rh@gmail.com>
2023-12-28 13:10:46 +0600

    updating practices

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# getting started
+# ***getting started
+
 # print("hello World")
 # x = (2 + 1)
 # print(x)
 
-# Variables
+# ***Variables
 
 # students_count = 1000
 # rating = 4.99
@@ -14,13 +15,66 @@
 # """
 # x, y = 1, 2
 
-# Dynamic Typing
+# ***Dynamic Typing
 # print(type(1.1))
 # print(type(123))
 # print(type(True))
 
-# Mutable and Immutable Types:\
-x = [1, 2, 3]
-print(id(x))
-x.append(4)
-print(id(x))
+# ***Mutable and Immutable Types:
+# x = [1, 2, 3]
+# print(id(x))
+# x.append(4)
+# print(id(x))
+
+# ***Strings
+
+# course = "Python Programming"
+# print(len(course))
+# print(course[0])
+# print(course[::-1])
+
+# ***Escape Sequences
+
+# \"
+# \'
+# \\
+# \n
+# msg = '''Python
+# is
+# a
+# great
+# programming
+# language'''
+# print(msg)
+
+# ***Formatted Strings
+
+# first = "Rabbi Hossain"
+# last = "Rakib"
+# full = first + " " + last
+# full2 = f"{first} {last}"
+# print(full)
+# print(full2)
+
+# ***Usefull String Methods
+
+# course = "Python Programming"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())  # to remove extra unwanted space
+# print(course.find("Pro"))
+# print(course.replace("P", "-"))
+
+# print("Programming" in course)
+# print("Programming" not in course)
+
+# *** Numbers
+
+x = 1023
+print(bin(x))
+print(hex(x))
+print(oct(x))
+
+y = 1+2j  # j is imaginary i in python
+print(y)
