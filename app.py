04952bac65c1f6f6093c7ad234c10f9c5fb028bc
Rabbi Hossain Rakib <rrakibb.rh@gmail.com>
2023-12-28 10:12:56 +0600

    Updating practices

--- app.py
+++ app.py
@@ -5,11 +5,22 @@
 
 # Variables
 
-students_count = 1000
-rating = 4.99
-is_published = False
-course_name = """
-Multiple 
-lines
-"""
-x, y = 1, 2
+# students_count = 1000
+# rating = 4.99
+# is_published = False
+# course_name = """
+# Multiple
+# lines
+# """
+# x, y = 1, 2
+
+# Dynamic Typing
+# print(type(1.1))
+# print(type(123))
+# print(type(True))
+
+# Mutable and Immutable Types:\
+x = [1, 2, 3]
+print(id(x))
+x.append(4)
+print(id(x))
